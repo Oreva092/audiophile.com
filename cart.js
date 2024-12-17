a1c0bb9f57c0398ca2591356095aa595a6b73ebf
Oreva Edgebe <danieledegbe81@gmail.com>
2024-12-17 06:05:32 -1000

    Add files via upload

---
+++ cart.js
@@ -0,0 +1,9 @@
+function radioFunction(){
+    document.querySelector(".radio").style.border = "1.7px solid rgb(141, 52, 0)";
+    document.querySelector(".radio").style.transition = '0.3s';
+}
+
+function radioConFunction(){
+    document.querySelector(".radioCon").style.border = "1.7px solid rgb(141, 52, 0)";
+    document.querySelector(".radio").style.transition = '0.3s';
+}
---
+++ home.js
@@ -0,0 +1,6 @@
+function menuLink(){
+    document.querySelector(".header").style.display = "none";
+    document.querySelector(".navLink").style.display = "block";
+    document.querySelector(".navLink").style.display = "flex";
+    document.querySelector(".menuBtn").style.display = "none";
+}
